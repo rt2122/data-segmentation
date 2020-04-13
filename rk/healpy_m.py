b6ceaa9fbfd9efe1c9fdec8ae040fc8f4b7c050c
rt2122 <alisa.nemeshaeva@gmail.com>
2020-04-13 21:53:10 +0300

    automatic

--- rk/healpy_m.py
+++ rk/healpy_m.py
@@ -1,3 +1,10 @@
+import healpy as hp
+import pandas as pd
+import numpy as np
+import astropy
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 def obj_in_pix(nside, ra, dec):
     from math import pi, modf
     sc = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='icrs') 
